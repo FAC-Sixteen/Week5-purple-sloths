2f119a52a5b3d2b5275aa537e723ed6fc0300160
Freddie <freddiepreece@outlook.com>
2019-04-04 11:58:11 +0100

    Adds tests for 404 and public routes

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -2,13 +2,9 @@ const supertest = require('supertest');
 const test = require('tape');
 const router = require('../router');
 
-test('Initialise', (t) => {
-  let num = 2;
-  t.equal(num, 2, 'Should return 2');
-  t.end();
-});
+//Home route tests
 
-test('Home returns status of 200', (t) => {
+test('Home route returns status of 200', (t) => {
   supertest(router)
   .get('/')
   .expect(200)
@@ -32,3 +28,74 @@ test('Home returns the response', (t) => {
     t.end();
   })
 });
+
+
+//Public route tests
+
+test('style.css in public route returns status of 200', (t) => {
+  supertest(router)
+  .get('/public/style.css')
+  .expect(200)
+  .expect('Content-Type', /css/)
+  .end((err, res) => {
+    t.error(err);
+    t.equal(res.statusCode, 200, 'Should return 200');
+    t.end();
+  })
+})
+
+test('dom.js in public route returns status of 200', (t) => {
+  supertest(router)
+  .get('/public/dom.js')
+  .expect(200)
+  .expect('Content-Type', /javascript/)
+  .end((err, res) => {
+    t.error(err);
+    t.equal(res.statusCode, 200, 'Should return 200');
+    t.end();
+  })
+})
+
+test('public route returns the response for dom.js', (t) => {
+  supertest(router)
+  .get('/public/dom.js')
+  .expect(200)
+  .expect('Content-Type', /javascript/)
+  .end((err, response) => {
+    t.error(err);
+    t.equal(typeof(response.text), 'string', 'Should return a string');
+    t.end();
+  })
+});
+
+
+
+//404 route tests
+
+test('404 route returns status of 404', (t) => {
+  supertest(router)
+  .get('/elephants')
+  .expect(404)
+  .expect('Content-Type', /html/)
+  .end((err, res) => {
+    t.error(err);
+    t.equal(res.statusCode, 404, 'Should return 404');
+    t.end();
+  })
+})
+
+test('404 returns the response', (t) => {
+  supertest(router)
+  .get('/elephants')
+  .expect(404)
+  .expect('Content-Type', /html/)
+  .end((err, response) => {
+    t.error(err);
+    t.equal(typeof(response.text), 'string', 'Should return a string');
+    t.end();
+  })
+});
+
+
+
+
